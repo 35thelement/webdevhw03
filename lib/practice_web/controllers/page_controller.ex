55a480565a5e2abcf63b558fffa807bd17358d85
Ben Romine <elementnumber35@gmail.com>
2019-01-21 15:00:28 -0500

    Router, Page controller, and web pages for factor & palindrome

--- lib/practice_web/controllers/page_controller.ex
+++ lib/practice_web/controllers/page_controller.ex
@@ -17,10 +17,13 @@ defmodule PracticeWeb.PageController do
   end
 
   def factor(conn, %{"x" => x}) do
-    y = Practice.factor(x)
+    {x, _} = Integer.parse(x)
+    y = List.last(Practice.factor(x))
     render conn, "factor.html", x: x, y: y
   end
 
-  # TODO: Add an action for palindrome.
-  # TODO: Add a template for palindrome over in lib/*_web/templates/page/??.html.eex
+  def palindrome?(conn, %{"text" => text}) do
+    y = Practice.palindrome?(text)
+    render conn, "palindrome.html", text: text, y: y
+  end
 end
